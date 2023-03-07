cb3893e8cf5413e6ad86d9e770a716ba98d5a166
Artem Klushin <arklual1@gmail.com>
2023-03-07 14:34:12 +0300

    two types support

--- bot.py
+++ bot.py
@@ -20,75 +20,114 @@ app = Client("parser", api_id=API_ID, api_hash=API_HASH,
 
 @app.on_message(filters.chat(SOURCE_PUBLICS))
 async def new_channel_post(client, message):
-    global old_files, media, is_creating
-    if message.text is not None: return
-    if is_creating: 
+    channel = message.chat.username
+    for chl in publics:
+        if str(chl['name']) == str(channel):
+            channel = chl
+            break
+    if int(channel['type']) == 1:
+        global old_files, media, is_creating
+        if message.text is not None: return
+        if is_creating: 
+            await app.download_media(message)
+            files = os.listdir(directory)
+            for file in files:
+                if file not in old_files:
+                    media.append(os.path.join(directory, file))
+            old_files = os.listdir(directory)
+            return
+        is_creating = True
+        t = 0
+        while t < TIMEOUT and (await get_last_message(message.chat.id)).text is None:
+            await asyncio.sleep(5)
+            t += 5
+
+        message_text = ''
+        text = (await get_last_message(message.chat.id)).text
+        if text is not None:
+            message_text = str(channel['description'])+'\n'+text+'\nТелефон продавца: '+str(channel['provider_phone'])
+        else:
+            message_text = str(channel['description'])+'\nТелефон продавца: '+str(channel['provider_phone'])
         await app.download_media(message)
         files = os.listdir(directory)
         for file in files:
             if file not in old_files:
                 media.append(os.path.join(directory, file))
         old_files = os.listdir(directory)
-        return
-    is_creating = True
-    t = 0
-    while t < TIMEOUT and (await get_last_message(message.chat.id)).text is None:
-        await asyncio.sleep(5)
-        t += 5
-    channel = message.chat.username
-    for chl in publics:
-        if str(chl['name']) == str(channel):
-            channel = chl
-            break
-
-    message_text = ''
-    text = (await get_last_message(message.chat.id)).text
-    if text is not None:
-        message_text = str(channel['description'])+'\n'+text+'\nТелефон продавца: '+str(channel['provider_phone'])
-    else:
-        message_text = str(channel['description'])+'\nТелефон продавца: '+str(channel['provider_phone'])
-    await app.download_media(message)
-    files = os.listdir(directory)
-    for file in files:
-        if file not in old_files:
-            media.append(os.path.join(directory, file))
-    old_files = os.listdir(directory)
-    if media == []: return
-    vk = vk_api.VkApi(VK_LOGIN, VK_PASSWORD)
-    vk.auth()
-    upload = vk_api.VkUpload(vk)
-    photo_list = []
-    for m in media:
-        try:
-            photo = upload.photo_wall(photos=m, group_id=GROUP_ID)
-            photo_list.append(*photo)
-        except:
-            pass
-    attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
-    vk.method('wall.post', {
-        'owner_id': '-'+GROUP_ID,
-        'message': message_text,
-        'attachments': attachment,
-        'from_group': 1,
-    })
-    for m in media:
-        m = m.replace('.temp', '')
-        try:
-            os.remove(m)
-        except:
-            pass
-    old_files = os.listdir(directory)
-    media = []
-    is_creating = False
+        if media == []: return
+        vk = vk_api.VkApi(VK_LOGIN, VK_PASSWORD)
+        vk.auth()
+        upload = vk_api.VkUpload(vk)
+        photo_list = []
+        for m in media:
+            try:
+                photo = upload.photo_wall(photos=m, group_id=GROUP_ID)
+                photo_list.append(*photo)
+            except:
+                pass
+        attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
+        vk.method('wall.post', {
+            'owner_id': '-'+GROUP_ID,
+            'message': message_text,
+            'attachments': attachment,
+            'from_group': 1,
+        })
+        for m in media:
+            m = m.replace('.temp', '')
+            try:
+                os.remove(m)
+            except:
+                pass
+        old_files = os.listdir(directory)
+        media = []
+        is_creating = False
+    elif int(channel['type']) == 2:
+        global old_files, media
+        if message.text is None: return
+        message_text = str(channel['description'])+'\n'+message.text+'\nТелефон продавца: '+str(channel['provider_phone'])
+        messages = message.get_media_group()
+        for message in messages:
+            await app.download_media(message)
+        files = os.listdir(directory)
+        for file in files:
+            if file not in old_files:
+                media.append(os.path.join(directory, file))
+        if media == []: return
+        vk = vk_api.VkApi(VK_LOGIN, VK_PASSWORD)
+        vk.auth()
+        upload = vk_api.VkUpload(vk)
+        photo_list = []
+        for m in media:
+            try:
+                photo = upload.photo_wall(photos=m, group_id=GROUP_ID)
+                photo_list.append(*photo)
+            except:
+                pass
+        attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
+        vk.method('wall.post', {
+            'owner_id': '-'+GROUP_ID,
+            'message': message_text,
+            'attachments': attachment,
+            'from_group': 1,
+        })
+        for m in media:
+            m = m.replace('.temp', '')
+            try:
+                os.remove(m)
+            except:
+                pass
+        old_files = os.listdir(directory)
+        media = []
+        
  
 @app.on_message(filters.chat(PRIVATE_PUBLIC))
 async def post_request(client, message):
     db = Database()
     global SOURCE_PUBLICS, publics
     if'/add_channel' in message.text:
-        # FORMAT: /add_channel|channel_link|description|provider_name|phone_number
+        # FORMAT: /add_channel|channel_link|description|provider_name|phone_number|type
         data = message.text.split('|')
-        db.add_channel(data[1], data[2], data[3], data[4])
+        db.add_channel(data[1], data[2], data[3], data[4], data[5])
         SOURCE_PUBLICS = []
         publics = db.get_channel_list()
         for public in publics:
--- database.py
+++ database.py
@@ -4,8 +4,8 @@ class Database:
         self.__conn = sqlite3.connect('db.sqlite3')
         self.__cur = self.__conn.cursor()
     
-    def add_channel(self, name, description, provider_name, provider_phone):
-        self.__cur.execute("INSERT INTO channels (name, description, provider_name, provider_phone) VALUES(?, ?, ?, ?);", (name, description, provider_name, provider_phone))
+    def add_channel(self, name, description, provider_name, provider_phone, type):
+        self.__cur.execute("INSERT INTO channels (name, description, provider_name, provider_phone, type) VALUES(?, ?, ?, ?, ?);", (name, description, provider_name, provider_phone, type))
         self.__conn.commit() 
 
     def del_channel(self, name):
@@ -13,15 +13,16 @@ class Database:
         self.__conn.commit() 
     
     def get_channel_list(self):
-        self.__cur.execute('SELECT name, description, provider_name, provider_phone FROM channels')
+        self.__cur.execute('SELECT name, description, provider_name, provider_phone, type FROM channels')
         chnls =  self.__cur.fetchall()
         channels = []
         for channel in chnls:
-            (name, description, provider_name, provider_phone) = channel
+            (name, description, provider_name, provider_phone, type) = channel
             channels.append({
                 "name": name,
                 "description" : description,
                 "provider_name": provider_name,
-                "provider_phone": provider_phone
+                "provider_phone": provider_phone,
+                "type": type
             })
         return channels
